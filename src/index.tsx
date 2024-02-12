cf34f953edd1eef62353a00cf4a4a5b4e2adcd94
bally <108769285+ballyalley-o@users.noreply.github.com>
2024-02-13 01:36:04 +1300

    Delete Child and Parent components, add GuestList and Search components, and update document title

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
+import { GuestList } from './component/guest-list'
+import { Search } from './component/search-guest'
+import { GLOBAL } from './config'
 
 const el = document.getElementById('root')
 const root = ReactDOM.createRoot(el!)
-
 const App = () => {
+  useEffect(() => {
+    document.title = GLOBAL.APP_TITLE
+  }, [])
+
   return (
-    <div className=''>
-      <h1>Bobbiside</h1>
+    <div>
+      <Search />
     </div>
   )
 }
